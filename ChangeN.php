0437c8af956ee0c47cb01453bbf6832e5d836553
ayoub bouguerne <ay.bouguern@gmail.com>
2024-12-29 23:48:55 +0100

    test

--- ChangeN.php
+++ ChangeN.php
@@ -1,4 +1,6 @@
 <?php 
 header('Content-Type: application/json');
 
+$conn=new PDO("mysql:host=fdb1027.runhosting.com;dbname=4569437_medi",'4569437_medi','databasemedi123');
+
 echo json_encode(['message'=>"Marid a ete changer avec success "]);
